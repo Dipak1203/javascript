0cc9962001445c908f3088d059c37cb16efc2477
DipakDai <dipakkumal673@gmail.com>
2023-03-22 15:44:59 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,221 @@
+// console.log("Hello Learner");
+
+// What is data type in javascript
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Object
+// 5. Array
+
+// Variable
+
+// // let AND Const
+// let money = 4000; -> Number
+// let name = "Dipak"; -> String
+// let paisa = 3000;
+// paisa = money + paisa;
+// console.log("My name is " + name + " I have " + paisa )
+
+// Comment
+
+// Personal Details
+// int a = 10; float = 4.5 => C language
+// let name = "Dipak";
+// let age = 20;
+// let height = 5.2;
+// let weight = 47;
+// let address = "Rapti-2";
+
+// console.log(name + age + height + weight + address)
+
+// Operator
+
+// Assignment Operator => '='
+// let name = "Dipak";
+
+// Arithmetic Operator => '',+', '- '*', '/', '%'
+// 13 % 4 => 1
+
+// Calculator
+// let a = 80;
+// let b = 30;
+// let sum = a + b;
+// let diff = a - b;
+// let mul = a * b;
+// let div = a / b;
+// let mod = a % b;
+
+// console.log("The sum is " + sum);
+// console.log("The diff is " + diff);
+// console.log("The mul is " + mul);
+// console.log("The div is " + div);
+// console.log("The mod is " + mod);
+
+// Comparison  Operator => '==' and '==='
+
+// const a = 40;
+// const b = "40";
+
+// console.log(a === b)
+// const a = 40;
+// const b = 44;
+
+// console.log(a == b)
+
+
+
+// Conditional Statement
+// if statement, else statement, if else if, switch statement
+
+// let age = 15;
+
+// if(age > 18)
+// {
+//     console.log("You are voter")
+// }
+// else
+// {
+//     console.log("you are not voter")
+// }
+
+
+// Nested Loop
+// if(condition){
+//     // statement
+// }else if(condition){
+//     // statement
+// }else if(condition){
+//     statemen/.t
+// }else{
+// //     // statement
+// }
+
+
+// if condition
+// if(condition){
+//     statement
+// }
+
+// Even and odd number using if else statement
+
+//
+
+// Nested
+// let marks = 56;
+
+// if(marks > 20){
+//     console.log("You are fail")
+// }else if(marks > 40){
+//     console.log("You are just pass")
+// }else if(marks > 60){
+//     console.log("You are good")
+// }else if(marks > 80){
+//     console.log("Your are talent")
+// }else{
+//     console.log("You are excellent")
+// }
+
+
+
+// Nested
+// Sunday,Monday
+
+// let day = "lkadjflkjaf";
+
+// if(day === "Sunday"){
+//     console.log("Today is Sunday")
+// }else if(day === "Monday"){
+//     console.log("Today is Monday")
+// }else if(day === "Tuesday"){
+//     console.log("Today is Tuesday")
+// }else if(day === "Wednesday"){
+//     console.log("Today is Wednesday")
+// }else{
+//     console.log("Invalid Day")
+// }
+
+// Switch statement
+
+// let day = "Monday";
+
+// switch (day) {
+//     case "Sunday":
+//         console.log("Today is Sunday")
+//         break;
+//     case "Monday":
+//         console.log("Today is Monday")
+//         break;
+//     case "Tuesday":
+//         console.log("Today is Tuesday")
+//         break;
+//     case "Wednesday":
+//         console.log("Today is Wednesday")
+//         break;
+//     case "Thursday":
+//         console.log("Today is Thursday")
+//         break;
+//     case "Friday":
+//         console.log("Today is Friday")
+//         break;
+//     default:
+//         console.log("Invalid day")
+//         break;
+// }
+
+
+
+// Loop
+// For loop 
+// While loop 
+// Do while loop
+
+// For in loop
+// For of loop
+// ForEach loop 
+
+// for(initialize; condition; decre/incre){ statement }
+
+// 2 to 10;
+// print 1,3,5,7,9
+
+
+// let num = 2;
+// let even = 2;
+
+// for(let i = 1; i<= 5; i++){
+//     console.log(even);
+//     even = even + num;
+// }
+
+//print 2,4,6,8,10;
+
+// While Loop in Javascript
+
+// Math Function
+
+// console.log(Math.ceil(40.9))
+
+// console.log(Math.floor(30.8))
+
+// console.log(Math.max(30,50,23,40,66,23,1))
+// console.log(Math.min(30,50,23,40,66,23,1))
+
+
+// console.log(Math.random() * 10)
+
+// console.log(Math.floor(Math.random() * 7))
+
+
+// Task 1: 
+// print 1,4,7,10,13,17
+
+// Task 2:
+// print 4,9,13,17
+
+// Task 3
+// While loop -> To research
+
+// Task 4
+// Print Grade system using if else 
+// Print Grade system using switch 
+
